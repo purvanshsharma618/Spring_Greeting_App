24c381ca5bbf8fd3a5efadc67cc1b27f73a8db7f
Purvansh Sharma <purvanshsharma6182gmail.com>
2025-02-24 12:05:41 +0530

    [Purvansh Sharma]Add. UC2 features added

--- src/main/java/com/example/GreetingApp/controller/GreetingController.java
+++ src/main/java/com/example/GreetingApp/controller/GreetingController.java
@@ -11,6 +11,36 @@ import java.util.Map;
 @RequestMapping("/greeting")
 public class GreetingController {
 
+    //UC1
+    @GetMapping
+    public ResponseEntity<Map<String, String>> getGreeting() {
+        Map<String, String> response = new HashMap<>();
+        response.put("message", "Hello from GET method!");
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping
+    public ResponseEntity<Map<String, String>> postGreeting() {
+        Map<String, String> response = new HashMap<>();
+        response.put("message", "Hello from POST method!");
+        return ResponseEntity.ok(response);
+    }
+
+    @PutMapping
+    public ResponseEntity<Map<String, String>> putGreeting() {
+        Map<String, String> response = new HashMap<>();
+        response.put("message", "Hello from PUT method!");
+        return ResponseEntity.ok(response);
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Map<String, String>> deleteGreeting() {
+        Map<String, String> response = new HashMap<>();
+        response.put("message", "Hello from DELETE method!");
+        return ResponseEntity.ok(response);
+}
+
+    //UC2
     private final GreetingService greetingService;
 
     // Constructor Injection
@@ -19,7 +49,7 @@ public class GreetingController {
     }
 
     @GetMapping
-    public ResponseEntity<Map<String, String>> getGreeting() {
+    public ResponseEntity<Map<String, String>> getGreetings() {
         Map<String, String> response = new HashMap<>();
         response.put("message", greetingService.getGreetingMessage());
         return ResponseEntity.ok(response);
